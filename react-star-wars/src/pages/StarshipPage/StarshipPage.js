817ecf9febba0d853bb6b2bf5ab4ee185bb38120
John Trujillo <amriikk@gmail.com>
2020-05-27 02:12:12 -0500

    fix minor issues

---
+++ react-star-wars/src/pages/StarshipPage/StarshipPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './StarshipPage.css';
+
+const StarshipPage = (props) => {
+  
+  const starship = props.getStarship(props.match.params.idx);
+  
+  console.log(starship)
+  return (
+    <div className='starship-page'>
+      {starship ?
+        <section className='starship'>
+          <div className='line'>Name: {starship.name}</div>
+          <div className='line'>Model: {starship.model}</div>
+          <Link to='/'>←</Link>
+        </section>
+        :
+        <>...</>
+      }
+    </div>
+  );
+};
+
+export default StarshipPage;
